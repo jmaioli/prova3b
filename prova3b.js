90c60bdc55a4e09c2f168b115490443fbe98daf9
jmaioli <109625285+jmaioli@users.noreply.github.com>
2023-09-05 22:29:11 -0300

    Add files via upload

---
+++ prova3b.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("H1");
+
+//https://symbl.cc/pt/collections/star-symbols/
+//https://getbootstrap.com/docs/5.3/getting-started/introduction/
+
+frm.addEventListener("submit", (e)  => {
+    e.preventDefault();
+
+    const lista = [];
+
+    for (let i = 0; i < lista[1]; i++) {      
+
+        form.innerText = '${lista}';
+
+
+        
+}
+)
